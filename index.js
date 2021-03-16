b8d48ba15b297729db1d77757b07629cdc5db90f
karenbarseghyan <karenbarseghyan90@gmail.com>
2021-03-17 01:21:08 +0400

    async & await

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //fetch api
 
+/*
 fetch("./name.json").then((response)=> {
     console.log(response);
     return response.json();
@@ -7,4 +8,21 @@ fetch("./name.json").then((response)=> {
     console.log(data);
 }).catch((err)=>{
     console.log(err);
-});
+});*/
+
+// async & await
+
+const getData = async () => {
+    const response = await fetch("./name.json")
+    const data = await response.json();
+    return data;
+    // const response1 = await fetch("./name1.json")
+    // const data1 = await response1.json();
+    // console.log(data1);
+};
+console.log(1);
+console.log(2);
+getData()
+    .then(data => console.log(data));
+console.log(3);
+console.log(4);
