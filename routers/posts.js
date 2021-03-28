86933afde71f038d70f09fda88d1a705bfb4e2fe
karenbarseghyan <karenbarseghyan90@gmail.com>
2021-03-29 00:57:58 +0400

    req.query, req.params & req.body

--- routers/posts.js
+++ routers/posts.js
@@ -1,19 +1,37 @@
 const express = require('express');
 const router = express.Router();
 
-let date = new Date();
-let hour = date.getHours();
-router.get('/', (req,res)=>{
-    res.end(`${hour} get`);
+router.route('/').get((req,res)=>{
+    res.end(new Date().toDateString());
+}).post((req,res)=>{
+    console.log(req.body.name);
+    console.log(req.body);
+    res.end(new Date().toDateString());
 });
-router.post('/', (req,res)=>{
-    res.end(`${hour} post`);
-});
-router.put('/', (req,res)=>{
-    res.end(`${hour} put`);
-});
-router.delete('/', (req,res)=>{
-    res.end(`${hour} delete`);
-})
+//
+// router.get('/', (req,res)=>{
+//     // console.log(req.query.limit);
+//     // console.log(req.query);
+//     res.end(new Date().toDateString());
+// })
+// router.post('/', (req,res)=>{
+//     console.log(req.name);
+//     res.end(new Date().toDateString());
+// });
+// router.put('/', (req,res)=>{
+//     res.end(new Date().toDateString());
+// });
+// router.delete('/', (req,res)=>{
+//     res.end(new Date().toDateString());
+// })
+
+// router.route('/:id').get((req, res) => {
+//     console.log(req.params.id);
+//     res.end("get " + req.params.id);
+// }).put((req, res)=>{
+//     res.end("edit " + req.params.id);
+// }).delete((req, res)=>{
+//     res.end("delete " + req.params.id);
+// })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require('express');
 const router = require("./router");
 
 const app = express();
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 app.set('port', 3000);
 router(app);
 
